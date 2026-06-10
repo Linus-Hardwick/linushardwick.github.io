bc671a6a9948674bee2d93ae23ea90074b92ab41
Linus-Hardwick <72452544+Linus-Hardwick@users.noreply.github.com>
2026-06-10 12:52:33 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "C:\\Users\\linus\\Code\\web-projects\\first-website\\images\\download.jpg") {
+  if (mySrc === "images\download.jpg") {
     myImage.setAttribute("src", "https://charlotteslivelykitchen.com/wp-content/uploads/2016/07/Butter-11.jpg");
   } else {
-    myImage.setAttribute("src", "C:\\Users\\linus\\Code\\web-projects\\first-website\\images\\download.jpg");
+    myImage.setAttribute("src", "images\download.jpg");
   }
 });
