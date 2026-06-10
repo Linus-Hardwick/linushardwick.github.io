26c0e4dddb728aa57460fdb26ec77107edc75cd7
Linus-Hardwick <72452544+Linus-Hardwick@users.noreply.github.com>
2026-06-10 12:57:24 +0100

    Fix escape characters in image source path

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images\download.jpg") {
+  if (mySrc === "images\\download.jpg") {
     myImage.setAttribute("src", "https://charlotteslivelykitchen.com/wp-content/uploads/2016/07/Butter-11.jpg");
   } else {
-    myImage.setAttribute("src", "images\download.jpg");
+    myImage.setAttribute("src", "images\\download.jpg");
   }
 });
